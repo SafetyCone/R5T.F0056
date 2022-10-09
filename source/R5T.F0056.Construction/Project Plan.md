38c699954b15600432b37c8d45196f2b2c82c857
davidcoats <david@getsafetycone.com>
2022-10-09 00:53:39 -0700

    Initial commit.

---
+++ source/R5T.F0056.Construction/Project Plan.md
@@ -0,0 +1,2 @@
+# R5T.F0056.Construction
+Construction console project for the R5T.F0056 library.
---
+++ source/R5T.F0056/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.F0056
+{
+	/// <summary>
+	/// Project operations functionality.
+	/// </summary>
+	public static class Documentation
+	{
+	}
+}
