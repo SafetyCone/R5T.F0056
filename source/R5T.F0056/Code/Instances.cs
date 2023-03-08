993fdaa45ec9a669cf20ac41822b6de5df1847df
davidcoats <david@getsafetycone.com>
2023-03-07 23:51:30 -0800

    Interim changes.

--- source/R5T.F0056/Code/Instances.cs
+++ source/R5T.F0056/Code/Instances.cs
@@ -1,26 +1,20 @@
 using System;
 
-using R5T.F0002;
-using R5T.F0020;
-using R5T.F0024;
-using R5T.F0041;
-using R5T.F0042;
-
 
 namespace R5T.F0056
 {
     public static class Instances
     {
-        public static ICommitMessages CommitMessages { get; } = F0042.CommitMessages.Instance;
-        public static IFileSystemOperator FileSystemOperator { get; } = F0056.FileSystemOperator.Instance;
-        public static IGitHubOperator GitHubOperator { get; } = F0041.GitHubOperator.Instance;
-        public static F0019.IGitOperator GitOperator { get; } = F0019.GitOperator.Instance;
-        public static ILicenseIdentifiers LicenseIdentifiers { get; } = F0056.LicenseIdentifiers.Instance;
-        public static IProjectFileOperator ProjectFileOperator { get; } = F0020.ProjectFileOperator.Instance;
-        public static F0051.IProjectOperator ProjectOperator { get; } = F0051.ProjectOperator.Instance;
-        public static IRepositoryPathsOperator RepositoryPathsOperator { get; } = F0042.RepositoryPathsOperator.Instance;
-        public static ISolutionOperations SolutionOperations { get; } = F0056.SolutionOperations.Instance;
-        public static ISolutionOperator SolutionOperator { get; } = F0024.SolutionOperator.Instance;
-        public static IStrings Strings { get; } = F0056.Strings.Instance;
+        public static Z0036.ICommitMessages CommitMessages => Z0036.CommitMessages.Instance;
+        public static IFileSystemOperator FileSystemOperator => F0056.FileSystemOperator.Instance;
+        public static F0041.IGitHubOperator GitHubOperator => F0041.GitHubOperator.Instance;
+        public static F0019.IGitOperator GitOperator => F0019.GitOperator.Instance;
+        public static ILicenseIdentifiers LicenseIdentifiers => F0056.LicenseIdentifiers.Instance;
+        public static F0020.IProjectFileOperator ProjectFileOperator => F0020.ProjectFileOperator.Instance;
+        public static F0051.IProjectOperator ProjectOperator => F0051.ProjectOperator.Instance;
+        public static F0042.IRepositoryPathsOperator RepositoryPathsOperator => F0042.RepositoryPathsOperator.Instance;
+        public static ISolutionOperations SolutionOperations => F0056.SolutionOperations.Instance;
+        public static F0024.ISolutionOperator SolutionOperator => F0024.SolutionOperator.Instance;
+        public static IStrings Strings => F0056.Strings.Instance;
     }
 }
